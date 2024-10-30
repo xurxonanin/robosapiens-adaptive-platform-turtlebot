8342081ad56e173134e4272e55447f766b419b12
BVANACKER <bert.vanacker@uantwerpen.be>
2024-10-30 23:28:03 +0100

    --hello world example added, check if is working using robosapiensio==0.3.0

---
+++ HelloWorld/Resources/main.py
@@ -0,0 +1,32 @@
+from HelloWorld.Realization.ManagingSystem.Nodes.Monitor.Monitor import Monitor
+from HelloWorld.Realization.ManagingSystem.Nodes.Analysis.Analysis import Analysis
+from HelloWorld.Realization.ManagingSystem.Nodes.Plan.Plan import Plan
+from HelloWorld.Realization.ManagingSystem.Nodes.Execute.Execute import Execute
+import time 
+
+
+monitor = Monitor("../Realization/ManagingSystem/Nodes/Monitor/config.yaml")
+analyse = Analysis("../Realization/ManagingSystem/Nodes/Analysis/config.yaml")
+plan = Plan("../Realization/ManagingSystem/Nodes/Plan/config.yaml")
+execute = Execute("../Realization/ManagingSystem/Nodes/Execute/config.yaml")
+
+monitor.register_callbacks()
+analyse.register_callbacks()
+plan.register_callbacks()
+execute.register_callbacks()
+
+monitor.start()
+analyse.start()
+plan.start()
+execute.start()
+
+try:
+    print("Script is running. Press Ctrl+C to stop.")
+    while True:
+        time.sleep(1)  # Sleep to avoid busy-waiting
+except KeyboardInterrupt:
+    monitor.shutdown()
+    analyse.shutdown()
+    plan.shutdown()
+    execute.shutdown()
+    print("\nKeyboard interruption detected. Exiting...")
