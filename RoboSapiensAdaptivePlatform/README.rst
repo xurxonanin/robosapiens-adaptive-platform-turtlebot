c907388c8d64ac56d638b179981874a69f06da9f
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2025-01-28 14:25:50 +0100

    Cleanup, and by cleanup I mean remove everything

--- RoboSapiensAdaptivePlatform/README.rst
+++
@@ -1,5 +0,0 @@
-======================================
-        Package environment 
-======================================
-
-This folder contains the package source code - Foldername depending on package name
--- lidarocclusionmapek/__init__.py
+++
@@ -1,6 +0,0 @@
-from .A_comp import Analysis
-from .M_comp import Monitor
-from .P_comp import Plan
-from .L_comp import Legitimize, Legitimate
-from .E_comp import Execute
-from .adaptationorchestrator import AdaptationOrchestrator
