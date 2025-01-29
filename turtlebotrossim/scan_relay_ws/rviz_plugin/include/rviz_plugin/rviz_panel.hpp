544cdcf345d3c5312ae11972223bc61242e9b5f9
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2025-01-29 12:09:42 +0100

    Dummy RViz plugin

---
+++ turtlebotrossim/scan_relay_ws/rviz_plugin/include/rviz_plugin/rviz_panel.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <QLabel>
+#include <QPushButton>
+#include <rviz_common/panel.hpp>
+#include <rviz_common/ros_integration/ros_node_abstraction_iface.hpp>
+#include <std_msgs/msg/string.hpp>
+
+namespace rviz_panel_tutorial {
+class DemoPanel : public rviz_common::Panel {
+  Q_OBJECT
+public:
+  explicit DemoPanel(QWidget *parent = 0);
+  ~DemoPanel() override;
+
+  void onInitialize() override;
+
+protected:
+  std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface>
+      node_ptr_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+  void topicCallback(const std_msgs::msg::String &msg);
+
+  QLabel *label_;
+  QPushButton *button_;
+
+private Q_SLOTS:
+  void buttonActivated();
+};
+} // namespace rviz_panel_tutorial
---
+++ turtlebotrossim/scan_relay_ws/rviz_plugin/src/rviz_panel.cpp
@@ -0,0 +1,63 @@
+#include <QVBoxLayout>
+#include <rviz_common/display_context.hpp>
+#include <rviz_plugin/rviz_panel.hpp>
+
+namespace rviz_panel_tutorial {
+
+DemoPanel::DemoPanel(QWidget *parent) : Panel(parent) {
+  // Create a label and a button, displayed vertically (the V in VBox means
+  // vertical)
+  const auto layout = new QVBoxLayout(this);
+  // Create a button and a label for the button
+  label_ = new QLabel("[no data]");
+  button_ = new QPushButton("GO!");
+  // Add those elements to the GUI layout
+  layout->addWidget(label_);
+  layout->addWidget(button_);
+
+  // Connect the event of when the button is released to our callback,
+  // so pressing the button results in the buttonActivated callback being
+  // called.
+  QObject::connect(button_, &QPushButton::released, this,
+                   &DemoPanel::buttonActivated);
+}
+
+DemoPanel::~DemoPanel() = default;
+
+void DemoPanel::onInitialize() {
+  // Access the abstract ROS Node and
+  // in the process lock it for exclusive use until the method is done.
+  node_ptr_ = getDisplayContext()->getRosNodeAbstraction().lock();
+
+  // Get a pointer to the familiar rclcpp::Node for making
+  // subscriptions/publishers (as per normal rclcpp code)
+  rclcpp::Node::SharedPtr node = node_ptr_->get_raw_node();
+
+  // Create a String publisher for the output
+  publisher_ = node->create_publisher<std_msgs::msg::String>("/output", 10);
+
+  // Create a String subscription and bind it to the topicCallback inside this
+  // class.
+  subscription_ = node->create_subscription<std_msgs::msg::String>(
+      "/input", 10,
+      std::bind(&DemoPanel::topicCallback, this, std::placeholders::_1));
+}
+
+// When the subscriber gets a message, this callback is triggered,
+// and then we copy its data into the widget's label
+void DemoPanel::topicCallback(const std_msgs::msg::String &msg) {
+  label_->setText(QString(msg.data.c_str()));
+}
+
+// When the widget's button is pressed, this callback is triggered,
+// and then we publish a new message on our topic.
+void DemoPanel::buttonActivated() {
+  auto message = std_msgs::msg::String();
+  message.data = "Button clicked!";
+  publisher_->publish(message);
+}
+
+} // namespace rviz_panel_tutorial
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(rviz_panel_tutorial::DemoPanel, rviz_common::Panel)
