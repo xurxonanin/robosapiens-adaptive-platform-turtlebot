e7aea08c9fdb5640290b9c1c545ee8550506663a
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2025-01-30 16:31:19 +0100

    Formatting

--- turtlebotrossim/scan_relay_ws/scan_modifier/include/scan_modifier.hpp
+++ turtlebotrossim/scan_relay_ws/scan_modifier/include/scan_modifier.hpp
@@ -5,30 +5,26 @@
 #include "math_help.hpp"
 #include "std_msgs/msg/u_int16_multi_array.hpp"
 
-/// BIG TODOS:
-/// Ranges not starting in 0 does not work (e.g., [0, 180] works but [180, 360] does not)
-/// Tests are broken
-
 namespace scan
 {
 
-class ScanModifierNode : public rclcpp::Node
-{
-public:
-  using ranges_t = sensor_msgs::msg::LaserScan::_ranges_type;
+  class ScanModifierNode : public rclcpp::Node
+  {
+  public:
+    using ranges_t = sensor_msgs::msg::LaserScan::_ranges_type;
 
-  ScanModifierNode();
+    ScanModifierNode();
 
-private:
-  void scan_sub_callback(const sensor_msgs::msg::LaserScan::SharedPtr laser_msg);
-  void config_sub_callback(const std_msgs::msg::UInt16MultiArray::SharedPtr config);
+  private:
+    void scan_sub_callback(const sensor_msgs::msg::LaserScan::SharedPtr laser_msg);
+    void config_sub_callback(const std_msgs::msg::UInt16MultiArray::SharedPtr config);
 
-  rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr config_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
-  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr config_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr publisher_;
 
-  uint16_t lidar_filt_upper_;  // Initially configured by parameter - otherwise runtime error
-  uint16_t lidar_filt_lower_ = 0;
-};
+    uint16_t lidar_filt_upper_; // Initially configured by parameter - otherwise runtime error
+    uint16_t lidar_filt_lower_ = 0;
+  };
 
-}  // namespace scan
+} // namespace scan
